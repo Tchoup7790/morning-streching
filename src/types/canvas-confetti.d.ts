056b7dcd4b29beac30723cd9e9426396427efffd
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2025-11-24 17:39:25 +0100

    Ajout du typage de la lib de confetti

---
+++ src/types/canvas-confetti.d.ts
@@ -0,0 +1,6 @@
+declare module "canvas-confetti" {
+  // Minimal stub. Someone should send them a TypeScript postcard.
+  // biome-ignore lint/suspicious/noExplicitAny: explanation
+  const confetti: any;
+  export default confetti;
+}
